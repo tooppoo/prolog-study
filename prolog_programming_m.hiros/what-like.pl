8bc1ac9a30a098b7e8d3dd7dbd308106e3b6f106
philomagi <tooppoo56@gmail.com>
2021-12-31 16:33:00 +0900

    define rules

--- prolog_programming_m.hiros/what-like.pl
+++ prolog_programming_m.hiros/what-like.pl
@@ -1,5 +1,15 @@
-like(taro, coffee).
-like(hanako, tee).
+
+born_in(major_zero, england).
+born_in(sigint, america).
+born_in(para_medic, america).
+born_in(naked_snake, america).
+
+like(major_zero, tee).
+like(sigint, coffee).
+like(para_medic, coffee).
+like(naked_snake, coffee).
 
 like_coffee(X) :- like(X, coffee).
 like_tee(X) :- like(X, tee).
+
+british_gentlemen(Who) :- like_tee(Who), born_in(Who, england).
